ac613ffb0a6737e919893ce656c2d5931e3ca7e0
osuss <osuss@yandex.com>
2023-11-10 09:34:41 +0000

    host_name_fix

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -8,7 +8,7 @@ SECRET_KEY = 'django-insecure-j_89af+30&&4qm*8z9_(^zz8p4-ho8z_m6ylm0s$h!-p@on1_^
 
 DEBUG = True
 
-ALLOWED_HOSTS = ['0.0.0.0', '10.8.0.1', '192.168.1.5', '127.0.0.1','taski.kochkam.site']
+ALLOWED_HOSTS = ['0.0.0.0', '10.8.0.1', '192.168.1.5', '127.0.0.1', 'taski.kochkam.site']
 
 
 # Application definition
